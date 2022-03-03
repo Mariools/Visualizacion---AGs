b8919da361c2c06edb99da442c678b592e9255c0
Mariools <mario.martin.lozano@gmail.com>
2022-03-03 20:29:38 +0100

    2 files modified

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ appDiv.innerHTML = `<h1>JS Starter</h1>`;
 //Gráfico de columnas. Mi 1º gráfico
 var visualization = d3plus.viz()
 .container("#viz1")
-.data([{"year": 1991, "name":"alpha", "value": 15},
-{"year": 1992, "name":"alpha", "value": 20},])
+.data([{"year": 1991, "name":"alpha", "value": 10},
+      {"year": 1992, "name":"alpha", "value": 20},
+      {"year": 1990, "name":"alpha", "value": 15},])
 .type("bar")
 .id("name")
 .x("year")
 .y("value")
 .draw()
+
